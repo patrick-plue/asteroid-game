cb356bf172dca060cabac72ee18bcfc840165c8a
Patrick Pluequett <patrick.pluequett@wbscodingschool.com>
2025-05-02 12:13:35 +0200

    implement rotation for spaceship

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        player.draw(screen)
-        
-
-
+                
         dt = clock.tick(60)
         dt /= 1000
+
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
 
         # call this last
         pygame.display.flip()
